ddf7224131a797d88db86c16a60bdf5384434422
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-12 22:24:36 +0300

    update

--- express-example-1/app.js
+++ express-example-1/app.js
@@ -1,15 +1,12 @@
-import express from "express";
-
-const app = express(); // app - web-server
-
+import express, { request, response } from "express";
+const app = express(); //app- веб сервер
 app.get("/", (request, response) => {
-  response.send("<h1>Home page</h1>");
+  response.send("<h1>Home Page</h1>");
 });
 
 app.get("/contacts", (request, response) => {
   console.log(request.url);
   console.log(request.method);
-  response.send("<h1>Contacts page</h1>");
+  response.send("<h1>Contacts Page</h1>");
 });
-
-app.listen(3000, () => console.log("Server running on 3000 PORT"));
+app.listen(4000, () => console.log("Server running on 4000 PORT"));
